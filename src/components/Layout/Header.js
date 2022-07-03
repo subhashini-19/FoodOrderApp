01c354d0b3eecaf4b6ee01a92d03c437b28234b9
Subhashini Dhamodaran <1910subhashini@gmail.com>
2022-07-03 12:40:50 +0530

    Header component
    Code cleanup

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,6 +1,6 @@
 import { Fragment } from "react";
 import mealsImage from "../../assets/meals.jpeg";
-import classes from './Header.module.css'
+import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
@@ -10,8 +10,8 @@ const Header = (props) => {
         <h1> Meals app</h1>
         <HeaderCartButton></HeaderCartButton>
       </header>
-      <div className={classes['main-image']}>
-        <img src={mealsImage} alt="A table full of food"/>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of food" />
       </div>
     </Fragment>
   );
