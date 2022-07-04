e1ca83bd619bc89c8720ddbdd2a0cda8cc70095c
Subhashini Dhamodaran <1910subhashini@gmail.com>
2022-07-04 11:48:38 +0530

    Controlling the model open and close actions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
+
+  const [showCart, setShowCart] = useState(false)
+
+  const showCartHandler = () => {
+    setShowCart(true);
+
+  }
+  const hideCartHandler = () => {
+   setShowCart(false);
+  }
+
   return (
     <Fragment>
-      <Cart/>
-       <Header></Header>
+      {showCart && <Cart onClose={hideCartHandler}/>}
+       <Header onShowCart={showCartHandler}></Header>
        <main>
          <Meals></Meals>
        </main>
